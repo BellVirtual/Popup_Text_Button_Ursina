c037aaf45e2f371cf2081f50d51b85ea590a3f03
BellVirtual <79067920+BellVirtual@users.noreply.github.com>
2021-02-15 17:22:15 -0600

    Initial Comment

---
+++ text button.py
@@ -0,0 +1,23 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+
+
+class Option(Button):
+    def __init__(self):
+        super().__init__(
+            model='circle',
+            color=color.blue,
+            scale=0.3)
+
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                popup_text = Text("Hello, Im gay!")
+                destroy(popup_text, delay=.3)
+
+
+demo_button = Option()
+
+app.run()
